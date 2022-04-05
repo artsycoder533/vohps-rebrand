af676fab0c9d4f4dd7f17271545858e6453d8377
Natasha <njohnson533@gmail.com>
2022-04-05 14:13:24 -0400

    added About component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,7 @@ import Home from "../../Pages/Home/Home";
 import Header from "../Header/Header";
 import { GlobalStyle } from './style';
 import Footer from "../Footer/Footer";
+import About from "../../Pages/About/About";
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <GlobalStyle />
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/services" />
-        <Route path="/faq" />
+        <Route path="/faqs" />
         <Route path="*" />
       </Routes>
       <Footer />
