d328d00cbd32cb45df91b1dd67e6ce1212ad7acd
Natasha <njohnson533@gmail.com>
2022-04-05 15:50:29 -0400

    added mobile view of about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Center } from "../../Components/App/style";
+import { Link } from "react-router-dom";
+import Headshot from "../../Images/headshot.jpg";
+import { BioStats, BioText } from "./style";
+import { PrimaryLink } from "../../Components/Button/style";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <main>
+      <Center>
+        <section>
+          <BioStats>
+            <img src={Headshot} alt="" />
+            <h3>Dr. Selisha Nelson-Smith PhD, LCP</h3>
+            <PrimaryLink to="/contact">Schedule Appointment</PrimaryLink>
+            <a href="">View Psychology Today Profile</a>
+          </BioStats>
+          <BioText>
+            <p>
+              I am a Licensed Clinical Psychologist with a passion for helping
+              others on their journeys to healing, self-discovery, and growth. I
+              have experience working with youth, adolescents and adults
+              providing individual, family, and couples therapy. Psychological
+              assessments are also provided. I have obtained advanced training
+              in trauma treatment, which includes various modalities such as
+              art/play therapy, Somatic Experiencing, and EMDR. I have over 14
+              years of experience working in community mental health,
+              alternative school settings, residential treatment, outpatient
+              settings, and correctional facilities.
+            </p>
+            <p>
+              I have clinical experience in the following modalities: Trauma
+              treatment, Somatic Experiencing, Cognitive-behavioral therapy,
+              Art/Play therapy, Dialectical Behavioral therapy (DBT),
+              Attachment, Couples/Marriage therapy, Eye Movement Desensitization
+              Reprocessing (EMDR), and Intensive evidence-based treatments for a
+              myriad of challenging mental health disorders.
+            </p>
+            <p>
+              I graduated in 2016 from Walden University with a Doctorate in
+              Clinical Psychology. I have a warm and gentle, yet, no nonsense
+              approach to therapy. I am passionate about helping others and
+              incorporate a fun, experiential, and creative approach to therapy.
+            </p>
+          </BioText>
+        </section>
+      </Center>
+    </main>
+  );
+};
 
-export default About
+export default About;
---
+++ src/Pages/About/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const BioText = styled.article`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    line-height: 1.5;
+    margin: 2rem 0;
+`;
+
+export const BioStats = styled.article`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    gap: 0.5rem;
+    margin-top: 2rem;
+`;
