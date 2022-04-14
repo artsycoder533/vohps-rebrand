bf52ad55fb1a6e125534378752b9ea6d0815def6
Natasha <njohnson533@gmail.com>
2022-04-14 12:59:02 -0400

    fixed height and added media query for title component

--- src/Components/Title/style.js
+++ src/Components/Title/style.js
@@ -2,22 +2,26 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     height: 150px;
+    height: auto;
     display: flex;
     justify-content: center;
 `;
 
 export const StyledTitle = styled.h2`
-    font-size: 3rem;
-    position: relative;
-    padding: 3rem 0;
+  font-size: 3rem;
+  position: relative;
+  padding: 3rem 0;
+  text-align: center;
 
-  &::after {
-    position: absolute;
-    content: "";
-    height: 4px;
-    width: 100%;
-    background: purple;
-    bottom: 25%;
-    left: 0;
+  @media all and (min-width: 600px) {
+    &::after {
+      position: absolute;
+      content: "";
+      height: 4px;
+      width: 100%;
+      background: purple;
+      bottom: 25%;
+      left: 0;
+    }
   }
 `;
